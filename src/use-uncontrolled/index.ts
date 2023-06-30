5934ed8f27f6a3acba4cb332b431c9e820fceeda
SukkaW <isukkaw@gmail.com>
2023-06-30 12:16:46 +0800

    refactor(use-uncontrolled): remove `useStableHandler` usage

--- src/use-uncontrolled/index.ts
+++ src/use-uncontrolled/index.ts
@@ -1,20 +1,28 @@
 import 'client-only';
-import { useCallback, useRef, useState } from 'react';
-import { useStableHandler } from '../use-stable-handler-only-when-you-know-what-you-are-doing-or-you-will-be-fired';
+import { useCallback, useReducer, useRef } from 'react';
 
 const identity = <V>(value: V) => value;
 
 /** @see https://foxact.skk.moe/use-uncontrolled */
 export function useUncontrolled<T, E extends HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement = HTMLInputElement>(initialValue: T, transformValue: (value: T) => T = identity) {
-  const [uncontrolledValue, setUncontrolledValue] = useState<T>(initialValue);
   const elementRef = useRef<E>(null);
-  const stableTransformValue = useStableHandler(transformValue);
+
+  // The dispatch is always memoized while reducer can always access the latest `transformValue` from prop
+  // https://overreacted.io/a-complete-guide-to-useeffect/#why-usereducer-is-the-cheat-mode-of-hooks
+
+  // Although the `transformValue` is most likely not stable, still wraps it with `useCallback` in case the
+  // user does memoize it and we are able to opt-in useReducer's internal optimization.
+  const reducer = useCallback((_prevState: T, valueAsAction: T) => {
+    return transformValue(valueAsAction);
+  }, [transformValue]);
+
+  const [uncontrolledValue, setUncontrolledValue] = useReducer(reducer, initialValue);
 
   const onCommitState = useCallback(() => {
     if (elementRef.current) {
-      setUncontrolledValue(stableTransformValue(elementRef.current.value as T));
+      setUncontrolledValue(elementRef.current.value as T);
     }
-  }, [stableTransformValue]);
+  }, []);
 
   return [uncontrolledValue, onCommitState, elementRef] as const;
 }
