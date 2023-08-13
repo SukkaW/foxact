517dc5ccd9963afb77ec91414c0fb7610a07261e
SukkaW <isukkaw@gmail.com>
2023-08-13 18:12:59 +0800

    fix(create-fixed-array): types

--- src/create-fixed-array/index.ts
+++ src/create-fixed-array/index.ts
@@ -1,6 +1,6 @@
-const arrayMap: Record<number, readonly unknown[]> = {};
+const arrayMap: Record<number, readonly number[]> = {};
 
-export const createFixedArray = (length: number): readonly unknown[] => {
+export const createFixedArray = (length: number): readonly number[] => {
   arrayMap[length] ||= arrayMap[length] || Array.from(Array(length).keys());
   return arrayMap[length];
 };
