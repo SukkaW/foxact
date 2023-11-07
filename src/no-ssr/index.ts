a4164ea351e38d91fe55f1075305105820802efd
SukkaW <isukkaw@gmail.com>
2023-11-07 22:11:59 +0800

    fix(no-ssr): only throw on the server

--- src/no-ssr/index.ts
+++ src/no-ssr/index.ts
@@ -1,12 +1,14 @@
 /** @see https://foxact.skk.moe/no-ssr */
 export const noSSR = (extraMessage?: string) => {
-  const error = new Error(extraMessage);
+  if (typeof window === 'undefined') {
+    const error = new Error(extraMessage);
 
-  // Next.js marks errors with `NEXT_DYNAMIC_NO_SSR_CODE` digest as recoverable:
-  // https://github.com/vercel/next.js/blob/ded28edeae16f8f8b4b9b117a83b5232e3623029/packages/next/src/client/on-recoverable-error.ts#L3
-  (error as any).digest = 'NEXT_DYNAMIC_NO_SSR_CODE';
+    // Next.js marks errors with `NEXT_DYNAMIC_NO_SSR_CODE` digest as recoverable:
+    // https://github.com/vercel/next.js/blob/ded28edeae16f8f8b4b9b117a83b5232e3623029/packages/next/src/client/on-recoverable-error.ts#L3
+    (error as any).digest = 'NEXT_DYNAMIC_NO_SSR_CODE';
 
-  (error as any).recoverableError = 'NO_SSR';
+    (error as any).recoverableError = 'NO_SSR';
 
-  throw error;
+    throw error;
+  }
 };
