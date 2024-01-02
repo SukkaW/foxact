4418cb4600a8b851f9cc63f7881e70e2c949c075
SukkaW <isukkaw@gmail.com>
2024-01-02 11:11:56 +0800

    refactor(invariant/nullthrow): add prefix to the error message

--- src/invariant/index.ts
+++ src/invariant/index.ts
@@ -1,5 +1,5 @@
 /** @see https://foxact.skk.moe/invariant-nullthrow */
-export function invariant<T>(value: T, message = 'Value is null or undefined'): asserts value is NonNullable<T> {
+export function invariant<T>(value: T, message = '[foxact/invariant] "value" is null or undefined'): asserts value is NonNullable<T> {
   if (value === null || value === undefined) {
     throw new TypeError(message);
   }
--- src/nullthrow/index.ts
+++ src/nullthrow/index.ts
@@ -1,7 +1,7 @@
 // TODO: https://github.com/microsoft/TypeScript/issues/40562
 
 /** @see https://foxact.skk.moe/invariant-nullthrow */
-export const nullthrow = <T>(value: T, message = 'Value is null or undefined'): NonNullable<T> => {
+export const nullthrow = <T>(value: T, message = '[foxact/invariant] "value" is null or undefined'): NonNullable<T> => {
   if (value === null || value === undefined) {
     throw new TypeError(message);
   }
