d2a16857e4672268f72a65302a56a72f880ac8aa
SukkaW <isukkaw@gmail.com>
2024-03-08 15:30:23 +0800

    fix(use-abortable-effect): add jsdoc

--- src/use-abortable-effect/index.ts
+++ src/use-abortable-effect/index.ts
@@ -1,6 +1,7 @@
 import 'client-only';
 import { type EffectCallback, useEffect as useEffectFromReact, type DependencyList } from 'react';
 
+/** @see https://foxact.skk.moe/use-abortable-effect */
 export const useAbortableEffect = (callback: (signal: AbortSignal) => ReturnType<EffectCallback>, deps: DependencyList) => {
   useEffectFromReact(() => {
     const controller = new AbortController();
