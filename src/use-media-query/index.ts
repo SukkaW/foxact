b643bd47d1c750f8d9818e0fb9b5a3f6e7eb3533
SukkaW <isukkaw@gmail.com>
2024-05-15 14:07:02 +0800

    feat(use-media-query): bring up the first version

---
+++ src/use-media-query/index.ts
@@ -0,0 +1,50 @@
+'use client';
+
+import { noSSRError } from '../no-ssr';
+import { noop } from '../noop';
+
+import { useCallback, useSyncExternalStore } from 'react';
+
+const externalStore = new Map<string, boolean>(); // External store to hold the state of each media query
+
+const subscribeToMediaQuery = (mq: string, callback: VoidFunction) => {
+  if (typeof window === 'undefined') return noop;
+
+  const mediaQueryList = window.matchMedia(mq);
+
+  const handleChange = () => {
+    // update the store with the latest value of a media query
+    externalStore.set(mq, mediaQueryList.matches);
+    callback();
+  };
+
+  mediaQueryList.addEventListener('change', handleChange); // Add change listener to MediaQueryList
+
+  return () => {
+    mediaQueryList.removeEventListener('change', handleChange); // Cleanup function to remove listener
+  };
+};
+
+const getServerSnapshotWithoutServerValue = () => {
+  throw noSSRError('useMediaQuery cannot be used on the server without a serverValue');
+};
+
+const useMediaQuery = (mq: string, serverValue?: boolean | undefined): boolean => {
+  if (typeof window !== 'undefined' && !externalStore.has(mq)) {
+    // This part of the code should only run once per media query, on client-side only
+    // we are not on the client-side, let's get initial value directly from DOM
+    externalStore.set(mq, window.matchMedia(mq).matches);
+  }
+
+  // subscribe once per hook per media query
+  const subscribe = useCallback((callback: VoidFunction) => subscribeToMediaQuery(mq, callback), [mq]);
+
+  const getSnapshot = () => externalStore.get(mq) ?? false;
+  const getServerSnapshot = serverValue !== undefined
+    ? () => serverValue
+    : getServerSnapshotWithoutServerValue;
+
+  return useSyncExternalStore(subscribe, getSnapshot, getServerSnapshot); // Use useSyncExternalStore to manage the subscription and state
+};
+
+export default useMediaQuery;
