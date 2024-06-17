ba489c480710ec8e72cd7b7357fccd9b6fc2f6a5
SukkaW <isukkaw@gmail.com>
2024-06-17 20:40:24 +0800

    perf(create-fixed-array): add gc support

--- src/create-fixed-array/index.ts
+++ src/create-fixed-array/index.ts
@@ -1,6 +1,27 @@
-const arrayMap: Record<number, readonly number[]> = {};
+const arrayMap = new Map<number, readonly number[]>();
+
+let finalizationRegistry: FinalizationRegistry<number> | undefined;
+if (typeof FinalizationRegistry === 'function') {
+  finalizationRegistry = new FinalizationRegistry((length: number) => {
+    arrayMap.delete(length);
+  });
+}
 
 export const createFixedArray = (length: number): readonly number[] => {
-  arrayMap[length] ||= arrayMap[length] || Array.from(new Array(length).keys());
-  return arrayMap[length];
+  if (arrayMap.has(length)) {
+    return arrayMap.get(length)!;
+  }
+
+  const arr = Array.from(new Array(length).keys());
+  if (process.env.NODE_ENV === 'development') {
+    Object.freeze(arr);
+  }
+
+  if (finalizationRegistry) {
+    finalizationRegistry.register(arr, length);
+  }
+
+  arrayMap.set(length, arr);
+
+  return arr;
 };
