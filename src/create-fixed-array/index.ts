7c4a7b3881f9a85b85bfeff107c7e21271a5e9ed
SukkaW <isukkaw@gmail.com>
2024-07-12 17:13:11 +0800

    fix(create-fixed-array): implement gc properly

--- src/create-fixed-array/index.ts
+++ src/create-fixed-array/index.ts
@@ -1,27 +1,42 @@
 const arrayMap = new Map<number, readonly number[]>();
 
-let finalizationRegistry: FinalizationRegistry<number> | undefined;
-if (typeof FinalizationRegistry === 'function') {
-  finalizationRegistry = new FinalizationRegistry((length: number) => {
-    arrayMap.delete(length);
-  });
-}
-
-export const createFixedArray = (length: number): readonly number[] => {
-  if (arrayMap.has(length)) {
-    return arrayMap.get(length)!;
-  }
-
+const makeArray = (length: number) => {
   const arr = Array.from(new Array(length).keys());
   if (process.env.NODE_ENV === 'development') {
     Object.freeze(arr);
   }
 
-  if (finalizationRegistry) {
-    finalizationRegistry.register(arr, length);
-  }
-
-  arrayMap.set(length, arr);
-
   return arr;
 };
+
+export const createFixedArrayWithoutGC = (length: number): readonly number[] => {
+  if (arrayMap.has(length)) {
+    return arrayMap.get(length)!;
+  }
+  const arr = makeArray(length);
+  arrayMap.set(length, arr);
+  return arr;
+};
+
+const arrayWeakRefMap = new Map<number, WeakRef<readonly number[]>>();
+
+export const createFixedArrayWithGC = (length: number): readonly number[] => {
+  let ref: WeakRef<readonly number[]> | undefined;
+  let array: readonly number[] | undefined;
+  if (arrayWeakRefMap.has(length)) {
+    ref = arrayWeakRefMap.get(length)!;
+    array = ref.deref();
+  }
+
+  if (!array) {
+    array = makeArray(length);
+
+    // every time a new array is created, we create a new WeakRef and update map
+    ref = new WeakRef(array);
+    arrayWeakRefMap.set(length, ref);
+  }
+
+  return array;
+};
+
+export const createFixedArray = typeof WeakRef === 'function' ? createFixedArrayWithGC : createFixedArrayWithoutGC;
