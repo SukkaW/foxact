f9349e0cc9bd0a8a03206d9ca6f1ae5d104ac9ef
Jack Works <5390719+Jack-Works@users.noreply.github.com>
2024-10-10 14:31:32 +0000

    fix: lint

--- src/use-component-will-receive-update/index.ts
+++ src/use-component-will-receive-update/index.ts
@@ -38,15 +38,15 @@ import { useState } from 'react';
  * @param deps
  */
 export function useComponentWillReceiveUpdate(callback: () => void, deps: readonly unknown[]) {
-    deps = [...deps]
-    const [prev, setPrev] = useState(deps)
-    let changed = deps.length !== prev.length
-    for (let i = 0; i < deps.length; i += 1) {
-        if (changed) break
-        if (prev[i] !== deps[i]) changed = true
-    }
-    if (changed) {
-        setPrev(deps)
-        callback()
-    }
+  deps = [...deps];
+  const [prev, setPrev] = useState(deps);
+  let changed = deps.length !== prev.length;
+  for (let i = 0; i < deps.length; i += 1) {
+    if (changed) break;
+    if (prev[i] !== deps[i]) changed = true;
+  }
+  if (changed) {
+    setPrev(deps);
+    callback();
+  }
 }
