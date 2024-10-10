11f6ba6dc27bb92de3323e5f44900003bcbd4038
Jack Works <5390719+Jack-Works@users.noreply.github.com>
2024-10-11 02:37:01 +0900

    Update index.ts

--- src/use-component-will-receive-update/index.ts
+++ src/use-component-will-receive-update/index.ts
@@ -1,42 +1,8 @@
 import { useState } from 'react';
 
 /**
- * If you're using useEffect like this:
- *
- * ```js
- * const [state, setState] = useState(false)
- * useEffect(() => setState(false), [props.someProp])
- * ```
- * Don't do it. See [Adjusting some state when a prop changes](https://react.dev/learn/you-might-not-need-an-effect#adjusting-some-state-when-a-prop-changes)
- * It should be like this:
- * ```js
- * const [prev, setPrev] = useState(state)
- * if (prev !== state) {
- *     setPrev(state)
- *     setState(false)
- * }
- * ```
- * This hook is a helper for the above pattern.
- * ```js
- * useComponentWillReceiveUpdate(() => setState(false), [state])
- * ```
- *
- * This only applies to states of the current component.
- * Modifying states from other components causes React reporting errors.
- * You may also want to read [(Avoid) Notifying parent components about state changes](https://react.dev/learn/you-might-not-need-an-effect#notifying-parent-components-about-state-changes)
- * and [(Avoid) Passing data to the parent](https://react.dev/learn/you-might-not-need-an-effect#passing-data-to-the-parent).
- * If you really need to edit other components' states, write it like this:
- *
- * ```js
- * useComponentWillReceiveUpdate(() => {
- *     setLocalState(false)
- *     Promise.resolve().then(() => props.setParentState(false))
- * }, [state])
- * ```
- *
- * @param callback
- * @param deps
- */
+  * @see {https://foxact.skk.moe/use-component-will-receive-update}
+  */
 export function useComponentWillReceiveUpdate(callback: () => void, deps: readonly unknown[]) {
   deps = [...deps];
   const [prev, setPrev] = useState(deps);
