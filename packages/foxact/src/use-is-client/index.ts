5edba42953bc0f8d6fb7f47bc2594fa5ccd264e8
SukkaW <isukkaw@gmail.com>
2025-11-28 01:51:58 +0800

    refactor(use-is-client): use `useSES` instead of `useEffect`

--- packages/foxact/src/use-is-client/index.ts
+++ packages/foxact/src/use-is-client/index.ts
@@ -1,16 +1,20 @@
 import 'client-only';
 
-import { useEffect, useState } from 'react';
+import { noop } from '../noop';
+import { useSyncExternalStore } from 'react';
+
+function trueFn() {
+  return true;
+}
+function falseFn() {
+  return false;
+}
 
 /** @see https://foxact.skk.moe/use-is-client */
 export function useIsClient() {
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    // This is only allowed because it won't trigger infinite re-render and double render is intentional
-
-    setMounted(true);
-  }, []);
-
-  return mounted;
+  return useSyncExternalStore(
+    noop,
+    trueFn,
+    falseFn
+  );
 }
